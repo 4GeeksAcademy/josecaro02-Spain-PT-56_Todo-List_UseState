eec830c25a5e1de7a2317ba108a496356ae0c8f4
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-02-14 18:20:35 +0000

    Manage inputs with useState

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//  const[variable,funcion que la modifica] = useState(valor por defecto)
+	// cuando la variable se actualiza el componente se re-renderiza    
+	const [name, setName] = useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5 container">
+			<div className="input-group flex-nowrap">
+				<span className="input-group-text" id="addon-wrapping">Mi nombre</span>
+				<input type="text" className="form-control" placeholder="Ingresa tu nombre"
+					aria-label="Username" aria-describedby="addon-wrapping"
+					value={name}
+					onChange={(e) => {
+						console.log(e.target.value)
+						setName(e.target.value)
+					}} />
+			</div>
+			<button className="btn btn-danger mt-3" onClick={() => {
+				setName("")
+			}}>Limpiar</button>
+			<h1>Mi nombre es: {name}</h1>
 		</div>
 	);
 };
